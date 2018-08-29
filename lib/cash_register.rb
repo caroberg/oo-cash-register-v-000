970a436aa3abe9c5ff0443629d4f5af25dd06ef6
Caroline Berg <carolineeberg@gmail.com>
2018-08-29 16:20:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,6 +3,9 @@ class CashRegister
 
   def initialize(total = 0)
     @total = total
-    @employee_discount = employee_discount
+  end
+
+  def employee_discount
+    @employee_discount
   end
 end
