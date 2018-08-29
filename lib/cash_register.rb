3dbe925520a409d16c9c28214950933c57511e56
Caroline Berg <carolineeberg@gmail.com>
2018-08-29 17:33:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,6 +23,13 @@ class CashRegister
     end
 
     def apply_discount
+      if @discount > 0
+            @discount = @discount/100.to_f
+             @total = @total - (@total * (@discount))
+             @total.to_i
+           else
+             @total
+           end
     end
 
 end
